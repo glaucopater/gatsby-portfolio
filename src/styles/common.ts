5a80f494563ce2a6f5513acc536e8cb678bbfea3
Glauco Pater <>
2020-05-22 19:50:33 +0200

    CSS Update, Add Index and Nero page

---
+++ src/styles/common.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+export const SCLink = styled(Link)`
+  background: #f8f9fa;
+  color: #111;
+  font-family: Virgil;
+  text-decoration: none;
+`;
+
+
+
+export const SCMain = styled.main`
+  padding: 1.45rem 1.0875rem 0;
+`;
+
