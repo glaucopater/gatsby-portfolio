5671067951f7bef7dc6498ee7765b8d088ff140e
Glauco Pater <>
2020-06-02 22:30:56 +0200

    Add Menu to other pages. Bump version.

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -29,7 +29,7 @@ export const SCCollapseMenu = styled.div<IProps>`
 
 export const SCMenuContent = styled.div<IProps>`
     transition: all 1s ease;
-    padding: 16px 0;
+    padding: 8px 0;
     ${props => props.isCollapsed ? "opacity: 0;" : "opacity: 1;"};
 `;
 
